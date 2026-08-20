901884336310c2258fecf59ae50faf53c5b055ba
Dishant Panchal <152392158+dishantpanchal03@users.noreply.github.com>
2026-08-20 19:49:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Insert in a Singly Linked List/insert-in-a-singly-linked-list.cpp
@@ -0,0 +1,42 @@
+/* Structure of Linked List Node
+class Node {
+  public:
+    int data;
+    Node *next;
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+    Node* insertPos(Node* head, int pos, int val) {
+        // code here
+        if(pos == 1){
+            Node* curr = new Node(val);
+            // curr->data = val;
+            curr->next = head;
+            return curr;
+        }
+        else{
+            Node* curr = head;
+            int n = 1;
+            while(n < pos-1){
+                curr = curr->next;
+                n++;
+            }
+            if(curr->next == NULL){
+            curr->next = new Node(val);
+            }
+            else{
+                Node* temp = curr->next;
+                curr->next = new Node(val);
+                curr = curr->next;
+                curr->next = temp;
+            }
+        }
+        return head;
+    }
+};
