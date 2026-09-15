d3b21608154e86cc608d48473f5b593ae303d068
Dishant Panchal <152392158+dishantpanchal03@users.noreply.github.com>
2026-09-15 19:35:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Substring with Distinct Characters/longest-substring-with-distinct-characters.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    int longestUniqueSubstr(string &s) {
+        // code here
+        vector<int> lastseen(256, -1);
+        
+        int left = 0, ans = 0;
+        
+        for(int right=0; right< s.size(); right++){
+            if(lastseen[s[right]] >= left){
+                left = lastseen[s[right]] + 1;
+            }
+            
+            lastseen[s[right]] = right;
+            ans = max(ans, right - left +1);
+        }
+        
+        return ans;
+    }
+};
